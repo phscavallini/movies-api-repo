fc80e98182f818b7e7cc274b025c9cd5e9bbc452
phscavallini <106893496+phscavallini@users.noreply.github.com>
2024-07-15 19:43:55 -0300

    Project setup w/ GCloud (#2)

--- src/main/java/dev/pauloh/movies/ReviewService.java
+++ src/main/java/dev/pauloh/movies/ReviewService.java
@@ -1,6 +1,5 @@
 package dev.pauloh.movies;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
